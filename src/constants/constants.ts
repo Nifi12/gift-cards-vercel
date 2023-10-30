91bebc1ac28c1b906fc327104a1269647ac6b65c
Mykhailo <mykhailo.rusyn@nixs.com>
2023-10-30 14:31:56 +0100

    added user

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -18,7 +18,7 @@ import type {
 	UserInter,
 } from '../@types/giftcards'
 
-export const API: string = import.meta.env.VITE_REACT_APP_API_KEY as string
+export const API = 'https://json-db-gift-cards-mock-versel.vercel.app/'
 export const CLIENT_ID: string = import.meta.env.VITE_REACT_GOOGLE_CLIENT_ID as string
 
 export const LogoPic = Logo
